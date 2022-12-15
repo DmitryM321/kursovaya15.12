5445cc6c180a9cc7b6def8e8a87888d70a83f5b2
Dmitry <a89297175356@gmail.com>
2022-12-15 18:29:48 +0500

    1

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+
 public class Main {
     static Employee[] employers = new Employee[10];
     public static void fullEmployersInfo() {   // Общая инф=ия о сотруднике
@@ -23,43 +24,38 @@ public class Main {
         }
         return sumSalary;
     }
-    public static int maxSalary() {
-        int max = 0;
-        int min = max;
-        for (Employee employee : employers) {
-            if (employee.getSalary() > max) {
-                max = employee.getSalary();
+    public static Employee maxSalary() {
+        Employee max = employers[0];
+           for (Employee employers : employers) {
+            if (employers.getSalary() > max.getSalary()) {
+                max = employers;
             }
         } return max;
     }
-    public static void minSalary() {
-        int min = maxSalary();
-        for (Employee employee : employers) {
-            if (employee.getSalary() < min) {
-                min = employee.getSalary();
+    public static Employee minSalary() {
+        Employee min = employers[0];
+        for (Employee employers : employers) {
+            if (employers.getSalary() < min.getSalary()) {
+                min = employers;
+
             }
-        }
-        System.out.println("Максимальная зарплата: " + maxSalary());
-        System.out.println("Минимальная зарплата: " + min);
-    }
-    public static int numberAllEmployers() {             // Число всех сотрудников
-        int allEmployee = employers.length;
-        return allEmployee;
+        } return min;
     }
     public static void averageSalary( ) {                 // Средняя ЗП
         int averageSalary;
-        averageSalary =  totalSalary()/numberAllEmployers() ;
+        averageSalary =  totalSalary()/employers.length ;
         System.out.println("Средняя зарплата : " + averageSalary);
     }
     public static void main (String[] args){
-        //        employers[0] = new Employee("Иванов Иван Иванович", 1, 100);
-        //        employers[1] = new Employee("Петров Петр Петрович", 2, 150);
-        //        employers[2] = new Employee("Ли Ван Клифф", 3, 400);
+           //      employers[0] = new Employee("Иванов Иван Иванович", 1, 100);
+           //      employers[1] = new Employee("Петров Петр Петрович", 2, 150);
+           //     employers[2] = new Employee("Ли Ван Клифф", 3, 400);
 
 
         fullEmployersInfo();
         System.out.println("Общая сумма зарплат: " +  totalSalary());
-        maxSalary( );
+        System.out.println("Максимальная зарплата: " + maxSalary());
+        System.out.println("Минимальная зарплата: " + minSalary());
         minSalary();
         averageSalary();
         infoOnlyFIO();
